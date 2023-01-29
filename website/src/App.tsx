2ca2d46c807b0ee3e8d83a22b1683562d2e1f767
getter111 <ayl@g.clemson.edu>
2023-01-28 20:22:43 -0500

    push react stuff

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header";
+import Card from "./components/Card";
+import CardsContainer from "./components/CardsContainer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CardsContainer>
+        <Card comment="hello" sentiment={1} symbol="AAPL" />
+        <Card comment="hello" sentiment={1} symbol="AAPL" />
+        <Card comment="hello" sentiment={1} symbol="AAPL" />
+
+        <Card comment="hello" sentiment={1} symbol="AAPL" />
+        <Card comment="hello" sentiment={1} symbol="AAPL" />
+      </CardsContainer>
     </div>
   );
 }
