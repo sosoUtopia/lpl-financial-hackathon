64f1b3764f052171076e4a2f398a915bbc75a28a
getter111 <ayl@g.clemson.edu>
2023-01-28 20:32:45 -0500

    react

--- api/alpaca_test.py
+++ api/alpaca_test.py
@@ -2,7 +2,9 @@ from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest, GetOrdersRequest
 from alpaca.trading.enums import OrderSide, TimeInForce, QueryOrderStatus
 from pprint import pprint
-
+import requests
+data = requests.get('http://127.0.0.1:8000/subreddit/wallstreetbets/?limit=1')
+pprint(data.json())
 trading_client = TradingClient('PKQC3TFPFRRETRPNQ46H', '3FU0CyoxGTPhIah10CP0LWVg3ldDN8XCVvhcWPQV', paper=True)
 account = trading_client.get_account()
 # pprint(account)
@@ -22,7 +24,7 @@ def buyStock(stockName, quantity):
    market_order = trading_client.submit_order(
                   order_data=market_order_data
                   )
-                  
+
 def sellStock(stockName, quantity): 
    # preparing the orders
    # params: qty:[double] -> amt to buy
@@ -39,4 +41,6 @@ def sellStock(stockName, quantity):
                   order_data=market_order_data
                   )
 
-
+# orders that satisfy params
+orders = trading_client.get_orders(filter=request_params)
+pprint(orders)
